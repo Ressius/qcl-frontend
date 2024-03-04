4bf04ab49c3a515e343dc152a49ec7f315aa74c0
Anthony Dumulong <adumulong@bkom.com>
2024-03-03 20:40:32 -0500

    Adding dist

--- dist/assets/SmallHeroHeader-291bc697.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as a,d as c,b as s,t as o,p as _,h as r}from"./index-7b8a9776.js";const d=e=>(_("data-v-44174cc8"),e=e(),r(),e),p={class:"greetings"},l=d(()=>s("div",{class:"fade"},null,-1)),n={__name:"SmallHeroHeader",props:{msg:{type:String,required:!0}},setup(e){return(i,m)=>(a(),c("div",p,[s("h1",null,o(e.msg),1),l]))}},S=t(n,[["__scopeId","data-v-44174cc8"]]);export{S};
