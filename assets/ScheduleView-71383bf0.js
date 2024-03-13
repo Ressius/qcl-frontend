b2aa1d5223f6b47a5ab2e6043295fcae0c00b03f
Anthony Dumulong <adumulong@bkom.com>
2024-03-12 23:00:36 -0400

    Adding dist

--- assets/ScheduleView-71383bf0.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./SmallHeroHeader-3133f7b5.js";import{r as t,o,c as r,w as c,a as e,b as n,S as _}from"./index-e2c19894.js";const l={class:"content"},h={__name:"ScheduleView",setup(m){return(d,p)=>{const a=t("stream");return o(),r(a,null,{default:c(()=>[e(s,{msg:"Horaire des Matchs"}),n("div",l,[e(_)])]),_:1})}}};export{h as default};
---
+++ assets/SmallHeroHeader-df093505.js
@@ -0,0 +1,1 @@
+import{_ as t,o as a,d as o,b as s,t as _,p as r,i as c}from"./index-e556605e.js";const d=e=>(r("data-v-b9d155e2"),e=e(),c(),e),p={class:"greetings"},l=d(()=>s("div",{class:"fade"},null,-1)),n={__name:"SmallHeroHeader",props:{msg:{type:String,required:!0}},setup(e){return(i,m)=>(a(),o("div",p,[s("h1",null,_(e.msg),1),l]))}},S=t(n,[["__scopeId","data-v-b9d155e2"]]);export{S};
