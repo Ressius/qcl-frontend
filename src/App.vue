82ed2985d63f0ab8a2337102d959968f46066e26
Anthony Dumulong <adumulong@bkom.com>
2024-03-17 15:13:58 -0400

    add footer component + fix footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import Navigation from './components/Navigation.vue'
+import Footer from './components/Footer.vue'
 import axios from 'axios'
 import { RouterLink, RouterView } from 'vue-router'
 import { store } from './store.js'
@@ -8,6 +9,7 @@ export default {
   name: 'App',
   components: {
     Navigation,
+    Footer,
     RouterLink,
     RouterView
   }
@@ -53,33 +55,8 @@ axios.get('https://qclservices.azurewebsites.net/tournament/get/7205060').then(r
 
 <template>
   <Navigation/>
-
   <RouterView />
-  <footer>
-    <div class="footer">
-    <div class="row">
-      <a href="https://www.twitch.tv/naniqc_"><i class="fa fa-brands fa-twitch"></i></a>
-      <a href="https://discord.gg/qcl"><i class="fa fa-brands fa-discord"></i></a>
-      <a href="https://www.youtube.com/@QCLigue"><i class="fa fa-brands fa-youtube"></i></a>
-      <a href="https://twitter.com/qclontop"><i class="fa fa-brands fa-x-twitter"></i></a>
-    </div>
-
-    <div class="row">
-        <ul>
-          <li><a href="/">Accueil</a></li>
-          <li><a href="/franchises">Franchises</a></li>
-          <li><a href="/equipe">L'équipe</a></li>
-          <li><a href="/horaire">Horaire</a></li>
-          <li><a href="/classement">Classement</a></li>
-          <li><a target="_blank" href="https://docs.google.com/document/d/1b95o_t9F86vNiQu5PbFwKcCbBzM3ShHhJmUGjqoRlTs/edit?usp=sharing">Règlements</a></li>
-          <li><a href="/stats">Statistiques</a></li>
-        </ul>
-    </div>
-    <div class="row">
-      QCL Copyright © 2024 Quebec Champions League - Tout droits réservés || Conçu par Anthony Dumulong
-    </div>
-  </div>
-</footer>
+  <Footer />
 </template>
 
 <style scoped>
@@ -87,58 +64,6 @@ axios.get('https://qclservices.azurewebsites.net/tournament/get/7205060').then(r
 body{
 margin:0;
 overflow-x:hidden;
-}
-
-footer{
-  padding:30px 0px;
-  font-family: 'GeneralSans', sans-serif;
-  text-align:center;
-  position: absolute;
-  width: 100%;
-  height: 2.5rem;
-  width: 100%;
-}
-
-.footer .row{
-width:100%;
-margin:1% 0%;
-padding:0.6% 0%;
-color:gray;
-font-size:0.8em;
-}
-
-.footer .row a{
-text-decoration:none;
-color:gray;
-transition:0.5s;
-font-size: 16px;
-}
-
-.footer .row a:hover{
-color:#fff;
-}
-
-.footer .row ul{
-width:100%;
-}
-
-.footer .row ul li{
-display:inline-block;
-margin:0px 30px;
-}
-
-.footer .row a i{
-font-size:1.1em;
-background-color: #0039D2;
-color: white;
-padding: 15px;
-border-radius: 35px;
-margin:0% 1%;
-}
-.footer .row a i:hover{
-  transition: 0.5s;
-  background-color: white;
-  color: black;
 }
 
 </style>
