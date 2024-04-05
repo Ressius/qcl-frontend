0db2a78d8ad55c0468137d36382b38038382a620
Anthony Dumulong <adumulong@bkom.com>
2024-04-05 16:56:15 -0400

    Adding dist

---
+++ dist/assets/ScheduleView-984b875d.js
@@ -0,0 +1,1 @@
+import{S as s}from"./SmallHeroHeader-c90a258f.js";import{r as t,o,c as r,w as c,a as e,b as n,S as _}from"./index-f44978f2.js";const l={class:"content"},h={__name:"ScheduleView",setup(m){return(d,p)=>{const a=t("stream");return o(),r(a,null,{default:c(()=>[e(s,{msg:"Horaire des Matchs"}),n("div",l,[e(_)])]),_:1})}}};export{h as default};
