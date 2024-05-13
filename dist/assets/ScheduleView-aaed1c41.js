fea24d99c7f73e819c257220612c5352fc7772c6
Anthony Dumulong <adumulong@bkom.com>
2024-05-13 13:40:47 -0400

    add stats from playoffs

---
+++ dist/assets/ScheduleView-aaed1c41.js
@@ -0,0 +1,1 @@
+import{S as s}from"./SmallHeroHeader-a47955c4.js";import{r as t,o,c as r,w as c,a as e,b as n,S as _}from"./index-0c9fdeda.js";const l={class:"content"},h={__name:"ScheduleView",setup(m){return(d,p)=>{const a=t("stream");return o(),r(a,null,{default:c(()=>[e(s,{msg:"Horaire des Matchs"}),n("div",l,[e(_)])]),_:1})}}};export{h as default};
